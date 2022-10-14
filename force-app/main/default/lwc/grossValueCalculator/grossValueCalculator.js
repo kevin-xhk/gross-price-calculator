633129289684f80ca32370f7b594f33e77b17921
ATREMA\mshkrepa <mshkrepa@deloittece.com>
2022-10-14 13:22:15 +0200

    Upgraded classes and lwc

--- force-app/main/default/lwc/grossValueCalculator/grossValueCalculator.js
+++ force-app/main/default/lwc/grossValueCalculator/grossValueCalculator.js
@@ -2,22 +2,59 @@
  * Created by mshkrepa on 10/12/2022.
  */
 
-import {LightningElement, track, wire} from 'lwc';
+import {LightningElement} from 'lwc';
 import getCountryCode from '@salesforce/apex/GrossValueCalculatorMethods.getCountryCode';
 import USER_ID from '@salesforce/user/Id';
 
 export default class GrossValueCalculator extends LightningElement {
-    resultValue;
 
-    @wire(getCountryCode, {id : USER_ID})
+    resultValue;
+    apiRate;
     countryCode;
 
     handleValueOnChange(event) {
-        this.netValue = parseInt(event.target.value);
+        this.netValue = parseFloat(event.target.value);
     }
 
-    handleValueChange() {
-        this.resultValue = this.countryCode.data;
+    //Get Country Code and then VAT Rate from API on component load
+    connectedCallback() {
+        getCountryCode({ userId: USER_ID })
+            .then(result => {
+                this.countryCode = result;
+            })
+            .then(() => {
+                this.getVatRateFromApi();
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    getVatRateFromApi() {
+        let requestOptions = {
+            method: 'GET',
+            headers: {"apikey": "rBSvEuVaGBTw8NrOx6ulwQu6gCeHfitJ", "Content-Type": "application/json"}
+        };
+        fetch("https://api.apilayer.com/tax_data/tax_rates?country=" + this.countryCode, requestOptions)
+            .then((response) => {
+                return response.json();
+            })
+            .then((jsonResponse) => {
+                let Standard_Rate = {
+                    rate: '',
+                };
+                let exchangeData = jsonResponse['standard_rate'];
+                Standard_Rate.rate = exchangeData['rate'];
+                this.apiRate = Standard_Rate.rate;
+            })
+            .catch(error => {
+                console.log('callout error ===> ' + JSON.stringify(error));
+            })
+
+    }
+
+    handleButtonClick() {
+        this.resultValue = this.netValue+(this.netValue*this.apiRate);
     }
 
 }
